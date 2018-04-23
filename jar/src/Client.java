6e2d3656767c36c3f527ca6875c718872f763ebc
luizfilho <luiizfiilhoo@gmail.com>
2018-04-23 12:27:57 -0400

    Main alterado

--- jar/src/Client.java
+++ jar/src/Client.java
@@ -5,19 +5,18 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Client implements Runnable{
-	
+
 	public void conectar() throws UnknownHostException, IOException {
 		DatagramSocket socket = new DatagramSocket();
 		socket.setBroadcast(true);
-		
 		String msg = "Opa, sou o cliente.";
 		byte[] buffer = msg.getBytes();
-		
+
 		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName("255.255.255.255"), 5555);
 		socket.send(packet);
 		socket.close();
 	}
-	
+
 	@Override
 	public void run() {
 		try {
