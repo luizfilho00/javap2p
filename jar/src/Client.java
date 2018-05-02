bef304b06ab7513be6f3c0fba2b051933ff28024
201619060353 <luiizfiilhoo@gmail.com>
2018-05-02 15:00:44 -0400

    Encontrei o cu do william

--- jar/src/Client.java
+++ jar/src/Client.java
@@ -1,64 +1,98 @@
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Client implements Runnable{
 
-	private HashSet<String> ipsConectados = new HashSet<>();
+    private HashSet<String> ipsConectados = new HashSet<>();
+    private HashMap<String, String[]> listaArquivos = new HashMap<String, String[]>();
 
-	public void listarUsuarios() throws UnknownHostException, IOException {
-		DatagramSocket socket = new DatagramSocket();
-		socket.setBroadcast(true);
-		String msg = "listarUsuarios";
-		byte[] buffer = msg.getBytes();
+    public void listarUsuarios() throws UnknownHostException, IOException {
+        DatagramSocket socket = new DatagramSocket();
+        socket.setBroadcast(true);
+        String msg = "listarUsuarios";
+        byte[] buffer = msg.getBytes();
 
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length,
-				InetAddress.getByName("255.255.255.255"), 5555);
-		socket.send(packet);
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length,
+                InetAddress.getByName("255.255.255.255"), 5555);
+        socket.send(packet);
 
-		//Recebe resposta
-		socket.receive(packet);
-		System.out.println("Recebeu resposta");
-		String ip = packet.getAddress().getHostAddress();
-		populaIpsConectados(ip);
-		socket.close();
-	}
+        //Recebe resposta
+        socket.receive(packet);
+        System.out.println("Recebeu resposta");
+        String ip = packet.getAddress().getHostAddress();
+        populaIpsConectados(ip);
+        socket.close();
+    }
 
-	//Cliente espera chegar algo na entrada = InputStream
-	public void buscarArquivo(String nomeArquivo) throws IOException, ClassNotFoundException {
+    //Cliente espera chegar algo na entrada = InputStream
+    public void buscarArquivo(String nomeArquivo) throws IOException, ClassNotFoundException {
         Socket socket = new Socket("localhost", 12002);
         Mensagem msg = new Mensagem("buscarArquivo");
         msg.setParam("nomeArquivo", nomeArquivo); //TODO Adaptar para buscar nome independente de extensão
-        ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
         ObjectOutputStream saida = new ObjectOutputStream(socket.getOutputStream());
         saida.writeObject(msg);
         saida.flush();
-        //ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
-        //boolean encontrouArquivo = (boolean)entrada.readObject();
+        ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
         boolean encontrouArquivo = entrada.readBoolean();
-        //socket.close();
         if (encontrouArquivo) System.out.println("Arquivo encontrado!");
         else System.out.println("Arquivo não encontrado :(");
+        entrada.close();
     }
 
-	private synchronized void populaIpsConectados(String ip){
-		if (ipsConectados.contains(ip) || ip.equals("127.0.0.1")) {
-			System.out.println("Ip ja existe na lista ou eh ip local");
-		}else{
-			ipsConectados.add(ip);
-		}
-	}
+    public void listarArquivos() throws IOException, ClassNotFoundException {
+        Socket socket = new Socket("localhost", 12002);
+        Mensagem msg = new Mensagem("listarArquivos");
+        ObjectOutputStream saida = new ObjectOutputStream(socket.getOutputStream());
+        saida.writeObject(msg);
+        saida.flush();
+        ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
+        String[] listaDeArquivos = (String[]) entrada.readObject();
+        if (listaDeArquivos.length < 1) System.out.println("Lista de arquivos vazia");
+        populaListaArquivos(socket.getInetAddress().getHostName(), listaDeArquivos);
+        entrada.close();
+    }
 
-	@Override
-	public void run() {
-		try {
-			listarUsuarios();
-			buscarArquivo("teste.pdf");
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+    private synchronized void populaListaArquivos(String ip, String[] lista){
+        listaArquivos.put(ip, lista);
+    }
+
+    private synchronized void populaIpsConectados(String ip){
+        if (ipsConectados.contains(ip) || ip.equals("127.0.0.1")) {
+            System.out.println("Ip ja existe na lista ou eh ip local");
+        }else{
+            ipsConectados.add(ip);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Entre com a opcao desejada:");
+            System.out.println("1 - Listar usuarios conectados na rede");
+            System.out.println("2 - Buscar arquivo pelo nome");
+            System.out.println("3 - Listar todos os arquivos da RCA");
+            BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+            String comando = leitor.readLine();
+            switch(comando){
+                case "1":
+                    listarUsuarios();
+                    break;
+                case "2":
+                    String nomeArquivo = leitor.readLine();
+                    buscarArquivo(nomeArquivo);
+                    break;
+                case "3":
+                    listarArquivos();
+                    break;
+                default:
+                    System.out.println("Deu ruim");
+                    break;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
--- jar/src/Server.java
+++ jar/src/Server.java
@@ -3,31 +3,31 @@ import java.net.*;
 import java.util.List;
 
 public class Server{
-	
-	private DatagramSocket serverSocketUDP;
-	private ServerSocket serverSocketTCP;
-	private byte[] buf = new byte[4096];
-	
-	public void criaConexaoUDP(int porta) throws IOException {
-		serverSocketUDP = new DatagramSocket(porta);
-	}
+
+    private DatagramSocket serverSocketUDP;
+    private ServerSocket serverSocketTCP;
+    private byte[] buf = new byte[4096];
+
+    public void criaConexaoUDP(int porta) throws IOException {
+        serverSocketUDP = new DatagramSocket(porta);
+    }
 
 
     public void criaConexaoTCP(int porta) throws IOException {
         serverSocketTCP = new ServerSocket(porta);
     }
-	
-	public void trataConexaoUDP() throws IOException {
+
+    public void trataConexaoUDP() throws IOException {
 
         while (true) {
-        	System.out.println("Esperando conexao UDP...");
+            System.out.println("Esperando conexao UDP...");
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             try {
-				serverSocketUDP.receive(packet);
-				System.out.println("Cliente UDP conectado!");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+                serverSocketUDP.receive(packet);
+                System.out.println("Cliente UDP conectado!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             InetAddress address = packet.getAddress();
             int port = packet.getPort();
@@ -39,27 +39,32 @@ public class Server{
             if (received.equals("listarUsuarios")) {
                 buf = new byte[4096];
                 listarUsuarios(packet, serverSocketUDP);
-                break;
             }
             buf = new byte[4096];
         }
-	}
+    }
 
-	public void trataConexaoTCP() throws IOException, ClassNotFoundException {
-        System.out.println("Esperando conexao TCP...");
-        Socket cliente = serverSocketTCP.accept();
-        System.out.println("Cliente TCP conectado: " + cliente.getInetAddress().getHostAddress());
-        ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-        ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-        Mensagem msg = (Mensagem) entrada.readObject();
+    public void trataConexaoTCP() throws IOException, ClassNotFoundException {
+        while(true){
+            System.out.println("Esperando conexao TCP...");
+            Socket cliente = serverSocketTCP.accept();
+            System.out.println("Cliente TCP conectado: " + cliente.getInetAddress().getHostAddress());
+            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+            ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+            Mensagem msg = (Mensagem) entrada.readObject();
 
-        if (msg.getOperacao().equals("buscarArquivo")){
-            boolean resultadoBusca = buscarArquivo((String)msg.getParam("nomeArquivo"));
-            saida.writeBoolean(resultadoBusca);
-            saida.close();
-            cliente.close();
-        }else{
-            System.out.println("Que porra recebeu nessa merda?");
+            if (msg.getOperacao().equals("buscarArquivo")){
+                boolean resultadoBusca = buscarArquivo((String)msg.getParam("nomeArquivo"));
+                saida.writeBoolean(resultadoBusca);
+                saida.close();
+                cliente.close();
+            }
+            else if (msg.getOperacao().equals("listarArquivos")){
+                String[] listaDeArquivos = listarArquivos();
+                saida.writeObject(listaDeArquivos);
+                saida.close();
+                cliente.close();
+            }
         }
     }
 
@@ -71,14 +76,31 @@ public class Server{
         datagramSocket.send(packet);
     }
 
-    public boolean buscarArquivo(String arquivo){
-        System.out.println("Chamado metodo para buscar aquivo: " + arquivo);
-        return true;
+    public synchronized String[] listarArquivos(){
+        File file = new File("rca");
+        if (!file.exists())
+            file.mkdir();
+        String[] arquivos = file.list();
+        System.out.println("Arquivos encontrados:");
+        for(String arq : arquivos)
+            System.out.println(arq);
+        return arquivos;
+    }
+
+    public boolean buscarArquivo(String nomeArquivoProcurado){
+        File file = new File("rca");
+        if (!file.exists()) return false;
+        String[] arquivos = file.list();
+        for(String arquivo : arquivos) {
+            if (arquivo.contains(nomeArquivoProcurado))
+                return true;
+        }
+        return false;
     }
 
     public boolean transferirArquivo(String arquivo){
-	    //TODO transferir arquivo via tcp
-	    return false;
+        //TODO transferir arquivo via tcp
+        return false;
     }
 
 
